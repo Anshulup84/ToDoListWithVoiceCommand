ceee53e693a87ec7b7ba40fb729243b358c839ec
Anshul <avenger720720@gmail.com>
2025-08-04 20:13:02 +0530

    working on functionality of recognition

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const taskList = []
 const listElement = document.getElementById("tasklist")
-const status = document.getElementById("status")
+const statusText = document.getElementById("status")
 
 // Speech Recognition
 
@@ -10,3 +10,14 @@ const recognition = new SpeechRecognition()
 
 recognition.continuous = false
 recognition.lang = 'en-US'
+
+recognition.onresult = (event) => {
+    const transcript = event.results[0][0].transcript.toLowerCase();
+    statusText.innerText = `Heard: "${transcript}"`;
+    if(transcript.startsWith("naya task")){
+        const taskText = transcript.replace("naya task", "").trim()
+        if(taskText)
+            addTask(taskText);
+    }
+}
+
